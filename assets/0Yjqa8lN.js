5fb4b31b56d85e8f3c281a1570bf44c97e771c40
soad <soad>
2025-12-12 19:14:12 +0200

    Updates

--- assets/0Yjqa8lN.js
+++
@@ -1,1 +0,0 @@
-import s from"./CF-rKC4q.js";import{s as r,O as u,v as c,f}from"./CeWAaJZJ.js";import"./B8IaAclc.js";const m=r({name:"Markdown",extends:s,setup(t){const{parent:n}=u(),{between:e,default:a}=c(),o=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:o,between:e,parent:n}}}),l=Object.assign(m,{__name:"Markdown"});export{l as default};
---
+++ assets/CZ2DnSd2.js
@@ -0,0 +1,1 @@
+import r from"./BG1Pitmj.js";import{v as s,J as u,x as c,g as m}from"./BsnxDntB.js";import"./B8IaAclc.js";const p=s({name:"Markdown",extends:r,setup(t){const{parent:n}=u(),{between:e,default:a}=c(),o=m(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:o,between:e,parent:n}}}),l=Object.assign(p,{__name:"Markdown"});export{l as default};
